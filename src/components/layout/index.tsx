9bc5146222d56fbdd7b12ca5fcd080e6b66295b7
Max <realmaxmeng@gmail.com>
2021-05-18 22:14:57 +1200

    Add Github Star

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,7 @@
 import { Layout as AntdLayout, Menu } from 'antd';
 import { Content, Footer, Header } from 'antd/lib/layout/layout';
 import { Calc } from '../calc';
+import './index.css'
 
 export const Layout = () => {
   return (
@@ -12,13 +13,21 @@ export const Layout = () => {
           <Menu.Item key="2">没什么用 2</Menu.Item>
           <Menu.Item key="3">没什么用 3</Menu.Item>
         </Menu>
+        <div className="github-star">
+          <iframe
+            src="https://ghbtns.com/github-btn.html?user=bonelol&repo=mhr-tool&type=star&count=true&size=large"
+            frameBorder="0"
+            scrolling="0"
+            width="170"
+            height="30"
+            title="GitHub"
+          ></iframe>
+        </div>
       </Header>
-      <Content style={{ padding: '0 50px', backgroundColor: '#fff' }}>       
+      <Content style={{ padding: '0 50px', backgroundColor: '#fff' }}>
         <Calc />
       </Content>
-      <Footer style={{ textAlign: 'center' }}>
-       Created by Bonelol
-      </Footer>
+      <Footer style={{ textAlign: 'center' }}>Created by Bonelol</Footer>
     </AntdLayout>
   );
 };
